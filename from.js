88353222d3adddaac8065a9b3119a8f78f83f33b
Akhilesh Rana <akhileshrana20@gmail.com>
2024-10-09 23:33:12 +0530

    initial commit

---
+++ from.js
@@ -0,0 +1,7 @@
+const array = "MANISH";
+let array1 = Array.from(array);
+console.log(array1);
+
+const arr = [1, 2, 3, 4, 5, 6];
+let double = Array.from(arr, (num) => num * 2);
+console.log(double);
